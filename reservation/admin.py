a5bebe3aaae9fb55563629cc8e489b821343817c
s8w1e2ep <ha9mv8c@gmail.com>
2022-01-15 21:17:55 +0800

    [feat] Build DB and admin backend page

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Admin, Room, Reservation
+
+admin.site.register(Admin)
+admin.site.register(Room)
+admin.site.register(Reservation)
--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Admin(models.Model):
+    user_id = models.CharField(max_length=20)
+    user_name = models.CharField(max_length=20)
+    create_time = models.DateTimeField('create time')
+    last_update_time = models.DateTimeField('last update time')
+
+    def __str__(self):
+        return f'{self.user_name} ({self.user_id})'
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=30)
+    create_time = models.DateTimeField('create time')
+    last_update_time = models.DateTimeField('last update time')
+
+    def __str__(self):
+        return self.room_name
+
+class Reservation(models.Model):
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    owner_id = models.CharField(max_length=20)
+    owner_name = models.CharField(max_length=20)
+    owner_department = models.CharField(max_length=20)
+    begin_time = models.DateTimeField('begin time')
+    end_time = models.DateTimeField('end time')
+    create_time = models.DateTimeField('create time')
+    last_update_time = models.DateTimeField('last update time')
+
+    def date_to_string(self, date):
+        return date.strftime('%Y-%m-%d (%a.) %H:%M:%S')
+
+    def __str__(self):
+        return f'{self.owner_department} - {self.owner_name} ({self.owner_id}) 預約 {self.room_id} 從 {self.date_to_string(self.begin_time)} 到 {self.date_to_string(self.end_time)}'
