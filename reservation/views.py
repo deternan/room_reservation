6e8fa52b448c70c01d3752ca605ff87d3f11bb7a
s8w1e2ep <ha9mv8c@gmail.com>
2022-01-16 20:37:24 +0800

    [feat] Build templates of login and index page

--- reservation/views.py
+++ reservation/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import Reservation
+
+import json
+
+def login(request):
+    return render(request, 'reservation/login.html')
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    reservation_list = list(Reservation.objects.order_by('begin_time')[:])
+    context = dict()
+    data = [
+        {
+            'id': elem.id,
+            'owner_id': elem.owner_id,
+            'name': elem.owner_name,
+            'department': elem.owner_department,
+            'start': elem.begin_time.isoformat(),
+            'end': elem.end_time.isoformat(),
+        }
+        for elem in reservation_list
+    ]
+    context['reservation'] = json.dumps(data)
+
+    return render(request, 'reservation/index.html', context)
