c39c9321dc64a4c89fd8ca75045da4b5eaee00e8
s8w1e2ep <ha9mv8c@gmail.com>
2022-01-23 13:44:33 +0800

    [feat] User can add event in frontend UI

--- code/reservation/static/reservation/index.js
+++ code/reservation/static/reservation/index.js
@@ -9,10 +9,16 @@ document.addEventListener('DOMContentLoaded', function () {
             end: elem.end
         })
     });
-
-    var calendarEl = document.getElementById('calendar');
-    var calendar = new FullCalendar.Calendar(calendarEl, {
+    var eventModal = new bootstrap.Modal(document.getElementById('eventModal'));
+    var calendarElem = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarElem, {
         initialView: 'dayGridMonth',
+        selectable: true,
+        selectMirror: true,
+        // Check if event is overlap
+        selectOverlap: function (event) {
+            return event.rendering === 'background';
+        },
         headerToolbar: {
             left: 'prev,next today',
             center: 'title',
@@ -29,6 +35,33 @@ document.addEventListener('DOMContentLoaded', function () {
         locale: 'zh-tw',
         firstDay: 1,
         events: events,
+        eventClick: function (info) {
+            var eventObj = info.event;
+            alert('Title: ' + eventObj.title);
+        },
+        businessHours: {
+            daysOfWeek: [1, 2, 3, 4, 5], // Monday - Friday
+            startTime: '08:00',
+            endTime: '22:00',
+        },
+        select: function (info) {
+            document.getElementById('name').value = 'IDD - 王曉明(21011234)';
+            document.getElementById('room').value = '第四研究室';
+            document.getElementById('start-time').value = info.startStr;
+            document.getElementById('end-time').value = info.endStr;
+            eventModal.show();
+        },
     });
+
+    var btnAdd = document.getElementById('btnAdd');
+    btnAdd.addEventListener('click', function () {
+        calendar.addEvent({
+            title: '開會',
+            start: document.getElementById('start-time').value,
+            end: document.getElementById('end-time').value,
+        });
+        eventModal.hide();
+    });
+
     calendar.render();
 });
