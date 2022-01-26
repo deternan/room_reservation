81b428b7c47ed49a50e8fc5b154c50d8c53a6ff4
s8w1e2ep <ha9mv8c@gmail.com>
2022-01-27 02:57:55 +0800

    [feat] Update DB models

--- code/reservation/admin.py
+++ code/reservation/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Admin, Room, Reservation
+from .models import Admin, Organization, Room, Reservation
 
 admin.site.register(Admin)
+admin.site.register(Organization)
 admin.site.register(Room)
 admin.site.register(Reservation)
--- code/reservation/models.py
+++ code/reservation/models.py
@@ -10,19 +10,33 @@ class Admin(models.Model):
     def __str__(self):
         return f'{self.user_name} ({self.user_id})'
 
-class Room(models.Model):
-    room_name = models.CharField(max_length=30)
+class Organization(models.Model):
+    department_code = models.CharField(max_length=20)
+    department_name = models.CharField(max_length=60)
     create_time = models.DateTimeField('create time')
     last_update_time = models.DateTimeField('last update time')
 
     def __str__(self):
-        return self.room_name
+        return f'{self.department_code} - {self.department_name}'
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=30)
+    room_department_code = models.CharField(max_length=20)
+    room_department = models.CharField(max_length=60)
+    manager_id = models.CharField(max_length=20)
+    manager = models.CharField(max_length=20)
+    create_time = models.DateTimeField('create time')
+    last_update_time = models.DateTimeField('last update time')
+
+    def __str__(self):
+        return f'{self.room_department_code} - {self.room_name}'
 
 class Reservation(models.Model):
     room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
-    owner_id = models.CharField(max_length=20)
-    owner_name = models.CharField(max_length=20)
-    owner_department = models.CharField(max_length=20)
+    meeting_name = models.CharField(max_length=60)
+    borrower_id = models.CharField(max_length=20)
+    borrower = models.CharField(max_length=20)
+    borrower_department_code = models.CharField(max_length=20)
     begin_time = models.DateTimeField('begin time')
     end_time = models.DateTimeField('end time')
     create_time = models.DateTimeField('create time')
@@ -32,4 +46,4 @@ class Reservation(models.Model):
         return date.strftime('%Y-%m-%d (%a.) %H:%M:%S')
 
     def __str__(self):
-        return f'{self.owner_department} - {self.owner_name} ({self.owner_id}) 預約 {self.room_id} 從 {self.date_to_string(self.begin_time)} 到 {self.date_to_string(self.end_time)}'
+        return f'{self.meeting_name}, {self.borrower_department_code} - {self.borrower} ({self.borrower_id}) 預約 {self.room_id} 從 {self.date_to_string(self.begin_time)} 到 {self.date_to_string(self.end_time)}'
