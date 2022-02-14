4894938449d81582e3ecb314920a7c3be625f926
s8w1e2ep <ha9mv8c@gmail.com>
2022-02-14 23:04:25 +0800

    [feat] Modify field of create_time and last_update_time to auto_new

--- code/reservation/models.py
+++ code/reservation/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from datetime import datetime
+from django.utils.timezone import localtime
 
 class Admin(models.Model):
     user_id = models.CharField(max_length=20)
     user_name = models.CharField(max_length=20)
-    create_time = models.DateTimeField('create time')
-    last_update_time = models.DateTimeField('last update time')
+    create_time = models.DateTimeField('create time', auto_now_add=True)
+    last_update_time = models.DateTimeField('last update time', auto_now=True)
 
     def __str__(self):
         return f'{self.user_name} ({self.user_id})'
@@ -13,8 +13,8 @@ class Admin(models.Model):
 class Organization(models.Model):
     department_code = models.CharField(max_length=20)
     department_name = models.CharField(max_length=60)
-    create_time = models.DateTimeField('create time')
-    last_update_time = models.DateTimeField('last update time')
+    create_time = models.DateTimeField('create time', auto_now_add=True)
+    last_update_time = models.DateTimeField('last update time', auto_now=True)
 
     def __str__(self):
         return f'{self.department_code} - {self.department_name}'
@@ -25,8 +25,8 @@ class Room(models.Model):
     room_department = models.CharField(max_length=60)
     manager_id = models.CharField(max_length=20)
     manager = models.CharField(max_length=20)
-    create_time = models.DateTimeField('create time')
-    last_update_time = models.DateTimeField('last update time')
+    create_time = models.DateTimeField('create time', auto_now_add=True)
+    last_update_time = models.DateTimeField('last update time', auto_now=True)
 
     def __str__(self):
         return f'{self.room_department_code} - {self.room_name}'
@@ -39,11 +39,12 @@ class Reservation(models.Model):
     borrower_department_code = models.CharField(max_length=20)
     begin_time = models.DateTimeField('begin time')
     end_time = models.DateTimeField('end time')
-    create_time = models.DateTimeField('create time')
-    last_update_time = models.DateTimeField('last update time')
+    create_time = models.DateTimeField('create time', auto_now_add=True)
+    last_update_time = models.DateTimeField('last update time', auto_now=True)
 
     def date_to_string(self, date):
-        return date.strftime('%Y-%m-%d (%a.) %H:%M:%S')
+        local_date = localtime(date)
+        return local_date.strftime('%Y-%m-%d (%a.)%H:%M:%S')
 
     def __str__(self):
         return f'{self.meeting_name}, {self.borrower_department_code} - {self.borrower} ({self.borrower_id}) 預約 {self.room_id} 從 {self.date_to_string(self.begin_time)} 到 {self.date_to_string(self.end_time)}'
